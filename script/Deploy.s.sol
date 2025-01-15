53f2d28b5bb0b4dac64ca3f294eaae314af153c3
Matt Burgess <matt@smallbatch.io>
2025-01-15 17:30:31 +0700

    Clear git state

---
+++ script/Deploy.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
+
+contract Deploy is Script {
+    function run() external {}
+}
